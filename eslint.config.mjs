e18dec8e9da05f039471ae5721c86513a5639317
Matteo Delton <deltonmatteo@gmail.com>
2024-08-25 09:42:58 +0200

    Add eslint-config-prettier dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
