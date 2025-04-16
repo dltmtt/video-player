58f58f596ed3ba5e144f80fcdbddff75d1785c96
Matteo Delton <deltonmatteo@gmail.com>
2025-04-16 08:58:47 +0200

    Refactor fetch event listener

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "video-player-pwa-cache-v6";
+const CACHE_NAME = "video-player-pwa-cache";
 const dynamicResourcesToCache = [
   "https://fonts.googleapis.com",
   "https://fonts.gstatic.com",
@@ -14,9 +14,7 @@ const staticResourcesToCache = [
 
 self.addEventListener("install", (e) => {
   // Filter out resources that are not served over HTTP, e.g., Chrome extension resources
-  if (!e.request?.url.startsWith("http")) {
-    return;
-  }
+  if (!e.request?.url.startsWith("http")) return;
 
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -28,40 +26,24 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
   const requestUrl = new URL(e.request.url);
 
-  if (dynamicResourcesToCache.includes(requestUrl.origin)) {
-    e.respondWith(
-      caches.open(CACHE_NAME).then(async (cache) => {
-        // Check if the request is already in the cache
-        const response = await cache.match(e.request);
-        if (response) {
-          return response;
-        }
+  e.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(e.request);
+      if (cachedResponse) return cachedResponse;
 
-        // If the request is not in the cache, fetch it and cache it
+      if (dynamicResourcesToCache.includes(requestUrl.origin)) {
         const networkResponse = await fetch(e.request);
-        cache.put(e.request, networkResponse.clone());
+        if (networkResponse?.ok) {
+          cache.put(e.request, networkResponse.clone());
+        }
         return networkResponse;
-      }),
-    );
-  } else {
-    // Return the resource from the cache if it exists, otherwise use preloaded response or fetch it
-    e.respondWith(
-      caches.match(e.request).then(async (response) => {
-        if (response) {
-          return response;
-        }
+      }
 
-        // Use preloaded response if available
-        const preloadResponse = await e.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        // Otherwise, fetch the resource
-        return fetch(e.request);
-      }),
-    );
-  }
+      const preloadedResponse = await e.preloadResponse;
+      return preloadedResponse || fetch(e.request);
+    })(),
+  );
 });
 
 self.addEventListener("activate", (e) => {
